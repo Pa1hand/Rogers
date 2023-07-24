a792a16171751a1e8ed622e8ac8a289ede403def
Pa1hand <140405411+Pa1hand@users.noreply.github.com>
2023-07-24 07:31:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_3lr4g1o", "template_9gkivtw", formData)
+    emailjs.send("service_0hd2jje", "template_ahms1x6", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
